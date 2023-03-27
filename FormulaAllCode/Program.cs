bbc897d338022d63f15c8520784625dc9a48bdad
416347 <416347@J1044>
2023-03-27 19:13:01 +0100

    Add project files.

---
+++ FormulaAllCode/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace FormulaAllCode
+{
+    class Program
+    {
+        static char portNumber;
+        static readonly byte MAX_SPEED = 100;
+        static readonly uint MAX_DISTANCE = 10_000;
+
+        static void Setup()
+        {
+            // Get User's port number via input
+            Console.Write("What is your port number: ");
+            byte portNumberByte;
+            while (!byte.TryParse(Console.ReadLine(), out portNumberByte))
+                Console.WriteLine("Retry with a valid port number.");
+            portNumber = (char)portNumberByte;
+
+            // Open Port
+            FA_DLL.FA_ComOpen(portNumber);
+
+            //while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+            {
+                // Playnote Test
+                FA_DLL.FA_PlayNote(portNumber, 500, 100);
+                FA_DLL.FA_PlayNote(portNumber, 100, 100);
+                FA_DLL.FA_PlayNote(portNumber, 600, 100);
+            }
+        }
+
+        static void SelfControl()
+        {
+            Console.WriteLine("Entering User Control Mode");
+            bool loop = true;
+            while (loop)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.W:
+                    case ConsoleKey.UpArrow:
+                        FA_DLL.FA_Forwards(portNumber, 100);
+                        break;
+                    case ConsoleKey.S:
+                    case ConsoleKey.DownArrow:
+                        FA_DLL.FA_Backwards(portNumber, 100);
+                        break;
+                    case ConsoleKey.A:
+                    case ConsoleKey.LeftArrow:
+                        FA_DLL.FA_Left(portNumber, 45);
+                        break;
+                    case ConsoleKey.D:
+                    case ConsoleKey.RightArrow:
+                        FA_DLL.FA_Right(portNumber, 45);
+                        break;
+                    case ConsoleKey.Escape:
+                        loop = false;
+                        break;
+                }
+            }
+        }
+
+        static void DragStrip()
+        {
+            byte speed;
+            Console.Write("Enter speed: ");
+            while (!byte.TryParse(Console.ReadLine(), out speed) || speed > MAX_SPEED)
+                Console.WriteLine("Retry with a valid speed (0 - {0}).", MAX_SPEED);
+            FA_DLL.FA_SetMotors(portNumber, speed, speed);
+            Console.WriteLine("Speed Set to {0}", speed);
+
+            uint distance;
+            Console.Write("Enter distance (Max {0}): ", MAX_DISTANCE);
+            while (!uint.TryParse(Console.ReadLine(), out distance) || distance > MAX_DISTANCE)
+                Console.WriteLine("Retry with a valid distance (0 - {0}).", MAX_DISTANCE);
+            Console.WriteLine("Distance Set to {0}", distance);
+
+            Console.WriteLine("Press Spacebar to go");
+            while (Console.ReadKey(true).Key != ConsoleKey.Spacebar) ;
+            FA_DLL.FA_Forwards(portNumber, distance);
+        }
+
+        static void Shutdown()
+        {
+            FA_DLL.FA_ComClose(portNumber);
+        }
+
+        static void Main(string[] args)
+        {
+            Setup();
+            SelfControl();
+            //DragStrip();
+            Forward(100);
+            Shutdown();
+            Console.ReadKey(true);
+        }     
+           
+        static void Forward(uint distance)
+        {
+            FA_DLL.FA_Forwards(portNumber, distance);
+        }
+    }
+}
+
+#region Adam's Old Stuff
+//bool quit = false;
+//do
+//{
+//    Console.WriteLine("What do you want me to do?");
+//    Console.WriteLine("A: Draw a House, B: Follow a Black line, C: Maze Runner");
+//    string option = Console.ReadLine();
+
+//    switch (option)
+//    {
+//        case "A":
+//        case "a":
+//            HouseDrawtest house = new HouseDrawtest();
+//            house.House();
+//            break;
+//        case "B":
+//        case "b":
+//            FollowTheLine line = new FollowTheLine();
+//            line.RaceTrack();
+//            break;
+//        case "C":
+//        case "c":
+//            mazeRunner runner = new mazeRunner();
+//            runner.MazeRunner();
+//            break;
+//        case "0":
+//            quit = true;
+//            break;
+//        default:
+//            Console.WriteLine("Invalid choice!");
+//            break;
+//    }
+//} while (quit == false);
+#endregion
---
+++ FormulaAllCode/mazeRunner.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaAllCode
+{
+    class mazeRunner
+    {
+        //Assign Port Number - Remember to change this to match your Formula AllCode port number
+        static char PortNumber = (char)9;
+
+        public void MazeRunner()
+        {
+            //Open Port
+            FA_DLL.FA_ComOpen(PortNumber);
+            //Send Play Note Command
+            FA_DLL.FA_PlayNote(PortNumber, 500, 100);
+            FA_DLL.FA_PlayNote(PortNumber, 100, 100);
+            FA_DLL.FA_PlayNote(PortNumber, 600, 100);
+
+
+            int clearance = 100;
+            int wall = 600;
+            while (true)
+            {                
+                uint left = FA_DLL.FA_ReadIR(PortNumber, (char)0);
+                uint front = FA_DLL.FA_ReadIR(PortNumber, (char)2);
+                //Console.WriteLine("Values = " + left + "\t" + front + "\t");
+                /*if (left <= wall && front <= clearance)
+                {
+                    FA_DLL.FA_SetMotors(PortNumber, 25, 25);
+                }*/
+
+                //check if enough left clearance and no obstical to the front to go foward
+                if (left <= wall) //break into nested ifs, Have alternatives for if its not <=
+                {
+                    if (front <= 200)
+                    {
+                        Console.WriteLine("A - move forward");
+                        // check if enough front clearance  
+                        FA_DLL.FA_SetMotors(PortNumber, 25, 25);
+                    }
+                    //clearance to left but obstical to front so turn right
+                    else
+                    {
+                        Console.WriteLine("B - clearance left and front obstacle - turn right");
+                        FA_DLL.FA_SetMotors(PortNumber, 0, 0);
+                        FA_DLL.FA_Right(PortNumber, 90);
+                    }
+                    // is there a left turn, if so turn left
+                    if (left < clearance)
+                    {
+                        Console.WriteLine("C - clearance at left so turn left");
+                        FA_DLL.FA_Forwards(PortNumber, 80);
+                        FA_DLL.FA_SetMotors(PortNumber, 0, 0);
+                        FA_DLL.FA_Left(PortNumber, 90);
+                    }
+                }
+                
+                //to close to left wall
+                else if (left >= wall)
+                {
+                    //and to close to front so turn right
+                    if (front >= wall)
+                    {
+                        Console.WriteLine("D - left / front walls so turn right");
+                        FA_DLL.FA_SetMotors(PortNumber, 0, 0);
+                        FA_DLL.FA_Right(PortNumber, 90);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ELSE - move forward");
+                    FA_DLL.FA_SetMotors(PortNumber, 25, 25);
+                }
+            }
+            //Close Port
+            FA_DLL.FA_ComClose(PortNumber);
+        }       
+    }
+}
